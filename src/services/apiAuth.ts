3a843ba396924d1e8c1bf861187b384b9a1df5c6
amuni27 <amanuuka@gm1il.com>
2025-12-28 19:08:14 -0600

    update all

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiAuth = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://agafari-menu-backend.onrender.com/api',
 });
 
 apiAuth.interceptors.request.use(
