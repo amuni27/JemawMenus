77b6758d4d8ca281c16caf9ae4f71c8bb3c9f6d4
amuni27 <amanuuka@gm1il.com>
2026-01-03 20:40:12 -0600

    update menu card sticky

--- src/pages/customer/CustomerMenu.tsx
+++ src/pages/customer/CustomerMenu.tsx
@@ -177,7 +177,7 @@ export default function CustomerMenu() {
                     <div ref={topSentinelRef}/>
 
                     {/* Sticky category bar (header scrolls normally, this stays visible) */}
-                    <div ref={stickyBarRef} className="sticky top-0 z-20 bg-white border-b border-gray-50 lg:top-[125px]">
+                    <div ref={stickyBarRef} className="sticky top-0 z-20 bg-white border-b border-gray-50 lg:top-[200px]">
                         {/* horizontally scrollable tabs (same UI look, just scrollable) */}
                         <div
                             ref={tabsBarRef}
