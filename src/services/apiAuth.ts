80c9df17ac470fa6cc1461795f72e18a2dc168ca
amuni27 <amanuuka@gm1il.com>
2026-02-18 21:47:28 -0600

    update api

--- src/services/apiAuth.ts
+++ src/services/apiAuth.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const apiAuth = axios.create({
-    baseURL: 'http://localhost:8080/api',
+    baseURL: 'https://jemaw-menu-backend-spring.onrender.com',
+    // baseURL: 'http://localhost:8080/api',
 });
 
 apiAuth.interceptors.request.use(
--- src/services/apiPublic.ts
+++ src/services/apiPublic.ts
@@ -2,6 +2,7 @@
 import axios from "axios";
 
 const apiPublic = axios.create({
-    baseURL: "http://localhost:8080",
+    // baseURL: "http://localhost:8080",
+    baseURL: "https://jemaw-menu-backend-spring.onrender.com",
 });
 export default apiPublic
